f4ff81c1a161c551d35bb3f29c6af9deeab05e60
meepleek <ergocrab@gmail.com>
2025-06-01 23:50:10 +0200

    debug: disable editor window for now

--- src/core/dev/editor.rs
+++ src/core/dev/editor.rs
@@ -4,20 +4,16 @@ use bevy_editor_pls::EditorWindowPlacement;
 use crate::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
-    app.configure::<IsEditorWindow>();
+    let mut window = Window {
+        title: "bevy_editor_pls".to_string(),
+        focused: false,
+        ..default()
+    };
+    window.set_minimized(true);
 
     let window = app
         .world_mut()
-        .spawn((
-            Name::new("EditorWindow"),
-            Window {
-                title: "bevy_editor_pls".to_string(),
-                focused: false,
-                visible: false,
-                ..default()
-            },
-            IsEditorWindow,
-        ))
+        .spawn((Name::new("EditorWindow"), window, IsEditorWindow))
         .id();
 
     app.add_plugins(EditorPlugin {
@@ -25,25 +21,6 @@ pub(super) fn plugin(app: &mut App) {
     });
 }
 
-const TOGGLE_KEY: KeyCode = KeyCode::F3;
-
 #[derive(Component, Reflect)]
 #[reflect(Component)]
 struct IsEditorWindow;
-
-impl Configure for IsEditorWindow {
-    fn configure(app: &mut App) {
-        app.register_type::<Self>();
-        app.add_systems(
-            Update,
-            toggle_editor_window.run_if(input_just_pressed(TOGGLE_KEY)),
-        );
-    }
-}
-
-#[cfg_attr(feature = "native_dev", hot)]
-fn toggle_editor_window(mut window_query: Query<&mut Window, With<IsEditorWindow>>) {
-    for mut window in &mut window_query {
-        window.visible ^= true;
-    }
-}
