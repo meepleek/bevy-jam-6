6567ab328de88fc84bb7fef678eb6779c2510ec4
meepleek <ergocrab@gmail.com>
2025-06-02 20:07:36 +0200

    feat: draw grids for boards

---
+++ src/core/dev/board.rs
@@ -0,0 +1,25 @@
+use bevy::ui::UiDebugOptions;
+
+use crate::game::board::Board;
+use crate::prelude::*;
+
+pub(super) fn plugin(app: &mut App) {
+    // todo: probly use states...
+    app.add_systems(Update, draw_board_grid);
+}
+
+#[cfg_attr(feature = "native_dev", hot)]
+fn draw_board_grid(options: Res<UiDebugOptions>, mut gizmos: Gizmos, board_q: Query<&Board>) {
+    if options.enabled {
+        for board in board_q {
+            gizmos
+                .grid_2d(
+                    Isometry2d::IDENTITY,
+                    board.size().as_uvec2(),
+                    Vec2::ONE * 96.,
+                    CYAN_400,
+                )
+                .outer_edges();
+        }
+    }
+}
