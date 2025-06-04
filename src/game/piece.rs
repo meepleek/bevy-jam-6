0f50fddb00cb442627e992890588740f36f285d8
meepleek <ergocrab@gmail.com>
2025-06-04 09:51:00 +0200

    debug: piece center uses a different colour

--- src/game/piece.rs
+++ src/game/piece.rs
@@ -4,6 +4,7 @@ use tiny_bail::or_continue;
 
 use super::PieceCoords;
 use super::board::TILE_SIZE;
+use crate::game::Coords;
 use crate::game::drag::Draggable;
 use crate::prelude::*;
 
@@ -149,22 +150,30 @@ fn on_piece_added(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    let explosion_mat = materials.add(Color::from(AMBER_200));
+    let center_mat = materials.add(Color::from(RED_200));
+
     for (e, piece) in added_piece_q {
         let mut e_cmd = or_continue!(cmd.get_entity(e));
         e_cmd.insert(Draggable);
 
         e_cmd.with_children(|b| {
             for tile in piece.explosion_tiles() {
+                let is_center = tile == PieceCoords::ZERO;
                 b.spawn((
                     Mesh2d(meshes.add(Rectangle::new(
                         TILE_SIZE as f32 * 0.9,
                         TILE_SIZE as f32 * 0.9,
                     ))),
-                    MeshMaterial2d(materials.add(Color::from(AMBER_300))),
+                    MeshMaterial2d(if is_center {
+                        center_mat.clone()
+                    } else {
+                        explosion_mat.clone()
+                    }),
                     Transform::from_xyz(
                         tile.x as f32 * TILE_SIZE as f32,
                         tile.y as f32 * TILE_SIZE as f32,
-                        0.0,
+                        if is_center { 0.2 } else { 0.1 },
                     ),
                     TilePieceOf(b.target_entity()),
                 ));
